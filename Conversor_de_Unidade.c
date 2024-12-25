aba30fc9dedf73b6bea3a67bdc4b508af72fc6a3
IkaroJS <Ikarojesusdasilva@outlook.com>
2024-12-25 18:30:59 -0300

    Correção de acentos

--- Conversor_de_Unidade.c
+++ Conversor_de_Unidade.c
@@ -13,16 +13,16 @@ int main()
 
     do
     {
-        printf("Escolha uma das conversıes:\n"
+        printf("Escolha uma das convers√µes:\n"
                "1 - Unidades de Comprimento\n"
                "2 - Unidades de Massa\n"
                "3 - Unidades de Volume\n"
                "4 - Unidades de Temperatura\n"
                "5 - Unidades de Velocidade\n"
                "6 - Unidades de Energia\n"
-               "7 - Unidades de ¡rea\n"
+               "7 - Unidades de √Årea\n"
                "8 - Unidades de Tempo\n"
-               "9 - Unidades de MemÛria\n"
+               "9 - Unidades de Mem√≥ria\n"
                "0 - Para sair!\n");
 
         scanf("%d", &tipoUnidade);
@@ -30,10 +30,10 @@ int main()
         switch (tipoUnidade){
         case 1: // Unidades de Comprimento
 
-            printf("Digite a unidade de origem (1. metro, 2. centÌmetro, 3. milÌmetro):\n");
+            printf("Digite a unidade de origem (1. metro, 2. cent√≠metro, 3. mil√≠metro):\n");
             scanf("%d", &unidadeOrigem);
 
-            printf("Digite a unidade de destino (1. metro, 2. centÌmetro, 3. milÌmetro):\n");
+            printf("Digite a unidade de destino (1. metro, 2. cent√≠metro, 3. mil√≠metro):\n");
             scanf("%d", &unidadeDestino);
 
             printf("Digite o valor a ser convertido:\n");
@@ -43,10 +43,10 @@ int main()
 
             if (resultado >= 0){
                 if ((unidadeOrigem == 1 && unidadeDestino == 2) || (unidadeOrigem == 2 && unidadeDestino == 3)){
-                    printf("O resultado da convers„o È: %.3lf\n", resultado);
+                    printf("O resultado da convers√£o √©: %.3lf\n", resultado);
                 }
                 else{
-                    printf("O resultado da convers„o È: %.3lf\n", resultado);
+                    printf("O resultado da convers√£o √©: %.3lf\n", resultado);
                 }
             }
                 printf("Pressione Enter para continuar...\n");
@@ -55,10 +55,10 @@ int main()
                 break;
 
             case 2: // Unidades de Massa
-                printf("\nDigite o n˙mero da unidade de origem (1. quilograma, 2. grama, 3. tonelada):\n");
+                printf("\nDigite o n√∫mero da unidade de origem (1. quilograma, 2. grama, 3. tonelada):\n");
                 scanf("%d", &unidadeOrigem);
 
-                printf("Digite o n˙mero da unidade de destino (1. quilograma, 2. grama, 3. tonelada):\n");
+                printf("Digite o n√∫mero da unidade de destino (1. quilograma, 2. grama, 3. tonelada):\n");
                 scanf("%d", &unidadeDestino);
 
                 printf("Digite o valor a ser convertido:\n");
@@ -68,11 +68,11 @@ int main()
 
                 if (resultado >= 0){
                 if ((unidadeOrigem == 1 && unidadeDestino == 3) || (unidadeOrigem == 2 && unidadeDestino == 1)) {
-                    printf("O resultado da convers„o È: %.4lf\n", resultado);
+                    printf("O resultado da convers√£o √©: %.4lf\n", resultado);
                 }else if((unidadeOrigem == 1 && unidadeDestino == 2) || (unidadeOrigem == 3 && unidadeDestino == 1) || (unidadeOrigem == 3 && unidadeDestino == 2)) {
-                    printf("O resultado da convers„o È: %.2lf\n", resultado);
+                    printf("O resultado da convers√£o √©: %.2lf\n", resultado);
                 }else{
-                    printf("O resultado da convers„o È: %.6lf\n", resultado);
+                    printf("O resultado da convers√£o √©: %.6lf\n", resultado);
                 }
             }
                 printf("Pressione Enter para continuar...\n");
@@ -85,7 +85,7 @@ int main()
                 break;
 
             default:
-                printf("Convers„o inv·lida");
+                printf("Convers√£o inv√°lida");
                 printf("Pressione Enter para continuar...\n");
                 while (getchar() != '\n');      // Limpa buffer
                 getchar(); // Aguarda Enter
